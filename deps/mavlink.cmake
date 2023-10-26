4809ec1858c0e6837a5647dcf0e3046e78029ab8
James Horner <james.horner@nrc-cnrc.gc.ca>
2023-10-26 13:39:40 -0400

    (Feat) Added options for configuring which mavlink repo/branch/dialect to use through cache variable configuration.

--- deps/mavlink.cmake
+++ deps/mavlink.cmake
@@ -1,4 +1,20 @@
+message(STATUS "Downloading Mavlink.")
+
 option(MAVLINK_GENERATED "Option for if the mavlink repo has dialect implementations in the generated/include directory or if it is in the root." ON)
+
+if(NOT DEFINED CACHE{MAVLINK_REPO})
+	message(WARNING	"Mavlink repository not set, defaulting open-source pre-generated Mavlink.")
+	set(MAVLINK_REPO 		"https://github.com/mavlink/c_library_v2.git")
+	set(MAVLINK_GENERATED	OFF)
+endif()
+if(NOT DEFINED CACHE{MAVLINK_REPO_TAG})
+	message(WARNING	"Mavlink repository tag not set, defaulting to master.")
+	set(MAVLINK_REPO_TAG	"master")
+endif()
+if(NOT DEFINED CACHE{MAVLINK_DIALECT})
+	message(WARNING	"Mavlink dialect not set, defaulting to common.")
+	set(MAVLINK_DIALECT		"common")
+endif()
 
 include(FetchContent)
 
@@ -19,3 +35,4 @@ if (MAVLINK_GENERATED)
 else()
 	set(mavlink_INCLUDE_DIR "${FETCHCONTENT_BASE_DIR}/mavlink-src/${MAVLINK_DIALECT}" CACHE STRING "Mavlink Include File Location")
 endif()
+message(STATUS "Downloaded Mavlink ${MAVLINK_REPO_TAG} (${MAVLINK_REPO}).")
